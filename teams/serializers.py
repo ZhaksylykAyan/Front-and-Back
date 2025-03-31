06e36d66ee9a02e062d17a25c9add578a4632170
Ayan <ay_zhaksylyk@kbtu.kz>
2025-04-01 03:02:10 +0500

    orders skills

--- teams/serializers.py
+++ teams/serializers.py
@@ -36,10 +36,15 @@ class TeamSerializer(serializers.ModelSerializer):
 class JoinRequestSerializer(serializers.ModelSerializer):
     team = TeamSerializer(read_only=True)
     student = StudentProfileSerializer(read_only=True)
+    team_members = serializers.SerializerMethodField()
 
     class Meta:
         model = JoinRequest
-        fields = ['id', 'team', 'student', 'status', 'created_at']
+        fields = ['id', 'team', 'student', 'status', 'created_at', 'team_members']
+
+    def get_team_members(self, obj):
+        members = obj.team.members.all()
+        return StudentProfileSerializer(members, many=True).data
 
 class SupervisorRequestSerializer(serializers.ModelSerializer):
     supervisor = SupervisorProfileSerializer(read_only=True)
